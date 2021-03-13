ad74404af63da7007296d2655b53e2a0c096ffb8
Pachciar <pachciar3@gmail.com>
2021-03-13 04:00:30 +0100

    Change Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
+// import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import Menu from './Menu';
@@ -42,7 +42,7 @@ function Header() {
           <Typography align="left" variant="h6" component="h1" className={classes.title}>
             Todo app
           </Typography>
-          <Button color="inherit">Login</Button>
+          {/* <Button color="inherit">Login</Button> */}
         </Toolbar>
       </AppBar>
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,7 +6,7 @@ function Home() {
   return (
     <div>
       <Typography variant="h3">Welcome in Todo App</Typography>
-      <Typography variant="subtitle1">Login and start using</Typography>
+      <Typography variant="subtitle1">This is Todo app created with using React, redux and material-ui</Typography>
     </div>
   );
 }
